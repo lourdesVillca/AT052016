e64b38303aeaa32344f26192fe165bba04cbb7ba
lourdesVillca <lourdes.villca@fundacion-jala.org>
2016-05-12 16:03:05 -0400

    Corrections in practice 6

--- Lourdes/Practice6/Passenger.rb
+++ Lourdes/Practice6/Passenger.rb
@@ -1,6 +1,6 @@
 def print (name,price,destination="Cbba")
 	price=price*6.96
-	puts "Nombre: #{name}, Price #{price}, Destination #{destination}"
+	puts "Nombre: #{name}, Price: #{price}, Destination: #{destination}"
 end
 
 puts "Ingrese nombre de usuario"
@@ -9,6 +9,13 @@ nombre = gets.chomp
 puts "Ingrese monto ticket"
 priceBs= gets.chomp.to_f
 
-print nombre,priceBs
-print nombre,priceBs,"La Paz"
+puts "Ingrese el destino"
+destination = gets.chomp.to_s
+
+
+
+if destination != "" then  print nombre,priceBs,destination else  print nombre,priceBs end
+
+
+
 
